73e01a3f5a13c39a5819eb0603769284e284bca9
jorgecoutinhobr <jorgecsn@id.uff.br>
2023-10-23 12:57:01 -0300

    adicionando-validações

--- app/helpers/unidades_helper.rb
+++ app/helpers/unidades_helper.rb
@@ -1,3 +1,9 @@
 module UnidadesHelper
-
+  def data_formatada(data) 
+    Date.parse(data).strftime("%d/%m/%Y") if data != ''
+  end
+  
+	def cep_formatado(cep)
+	  cep.split('').insert(5, '-').join
+	end
 end
