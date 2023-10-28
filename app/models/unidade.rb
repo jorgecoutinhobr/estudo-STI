db5f5797bbd9a5e5302bd5e0d67ff9c68cff76c8
jorgecoutinhobr <jorgecsn@id.uff.br>
2023-10-27 22:12:19 -0300

    commit-semana-3-final

--- app/models/unidade.rb
+++ app/models/unidade.rb
@@ -1,8 +1,3 @@
 class Unidade < ApplicationRecord
-  validates :nome, presence: true
-  validates :endereco, presence: true
-  validates :numero, presence: true
-  validates :bairro, presence: true
-  validates :cidade, presence: true
-  validates :cep, presence: true
+  validates :nome, :endereco, :numero, :bairro, :cidade, :cep, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  
   resources :unidades
   root 'welcome#index'
 end
