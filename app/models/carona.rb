6ef35b13581e807fb3e5f0318dc232cacc86f3eb
jorgecoutinhobr <jorgecsn@id.uff.br>
2023-10-31 21:48:55 -0300

    adicionando validações

--- app/models/carona.rb
+++ app/models/carona.rb
@@ -1,5 +1,16 @@
 class Carona < ApplicationRecord
   has_many :paradas, dependent: :destroy, inverse_of: :carona
   accepts_nested_attributes_for :paradas, allow_destroy: true, reject_if: :all_blank
-  validates :nome, :dia, :partida, :destino, :hora, :passageiros, :valor ,presence: true
+  validates :nome, :dia, :destino, :hora, :passageiros, :valor ,presence: true
+  validates_format_of :valor, with: /\A\d+(.\d{1,2})?\z/, message: "Valor até duas casas decimais"
+  validate :npassageiros
+  
+  private
+
+  def npassageiros
+    unless (1..4).include?(passageiros)
+      errors.add(:passageiros, "dem ser de no mínimo 1 e no máximo 4")
+    end
+  end
+
 end
