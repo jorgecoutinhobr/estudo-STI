e84c2ab99c78e27c771a886ff1c24735e016c570
jorgecoutinhobr <jorgecsn@id.uff.br>
2023-10-31 21:49:17 -0300

    Adicionando animação JS no formulario

--- app/javascript/packs/unidade_escolha.js
+++ app/javascript/packs/unidade_escolha.js
@@ -1,20 +1,16 @@
-$(document).ready(function() {
-    $('#unidade_tipo').change(function() {
-      var escolha = $(this).val();
-      if (escolha === 'partida') {
-        $('#partida_select').show();
-        $('#partida_input').hide();
-        $('#destino_select').hide();
-        $('#destino_input').show();
-      } else if (escolha === 'destino') {
-        $('#partida_select').hide();
-        $('#partida_input').show();
-        $('#destino_select').show();
-        $('#destino_input').hide();
-      }
-    });
-  
-    $('#unidade_tipo').trigger('change');
+document.addEventListener('turbolinks:load', function() {
+  $('#unidade_tipo').change(function() {
+    var escolha = $(this).val();
+    if (escolha === 'partida') {
+      $('#partida_select').prop('disabled', false).prop('hidden', false);
+      $('#partida_input').prop('disabled', true).prop('hidden', true);
+      $('#destino_select').prop('disabled', true).prop('hidden', true);
+      $('#destino_input').prop('disabled', false).prop('hidden', false);
+    } else if (escolha === 'destino') {
+      $('#partida_select').prop('disabled', true).prop('hidden', true);
+      $('#partida_input').prop('disabled', false).prop('hidden', false);
+      $('#destino_select').prop('disabled', false).prop('hidden', false);
+      $('#destino_input').prop('disabled', true).prop('hidden', true);
+    }
   });
-  
-  
+});
