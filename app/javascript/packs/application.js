452a3955ab533d580f88430c3884645c4ffd76e9
jorgecoutinhobr <jorgecsn@id.uff.br>
2023-10-31 21:48:35 -0300

    consertando require popperjs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,8 +10,8 @@ require("channels")
 require("jquery")
 require("bootstrap")
 require("@nathanvda/cocoon")
-require("popper.js")
-require("packs/unidade_escolha")
+require("@popperjs/core")
+
   
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
